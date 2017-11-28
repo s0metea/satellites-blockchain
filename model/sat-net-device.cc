125236894d4b901a94ef03c6460540cc9dea579d
sometea <sometea1@gmail.com>
2017-11-28 18:20:08 +0300

    Methods implementation

--- model/sat-net-device.cc
+++ model/sat-net-device.cc
@@ -1,6 +1,7 @@
 #include "sat-channel.h"
 #include "sat-net-device.h"
 #include <ns3/network-module.h>
+#include <ns3/pointer.h>
 
 namespace ns3 {
 
@@ -13,8 +14,23 @@ NS_OBJECT_ENSURE_REGISTERED (SatNetDevice);
     {
 		static TypeId tid = TypeId ("ns3::SatNetDevice")
 		.SetParent<NetDevice> ()
-		.SetGroupName ("NetDevice");
-	    return tid;
+		.SetGroupName ("NetDevice")
+        .AddAttribute ("DataRate",
+                       "The data rate that the Net Device uses to simulate packet transmission timing.",
+                       DataRateValue (DataRate ("1250Mb/s")),
+                       MakeDataRateAccessor (&SatNetDevice::m_bps),
+                       MakeDataRateChecker ())
+                .AddAttribute ("Mtu", "The MAC-level Maximum Transmission Unit",
+                               UintegerValue (DEFAULT_MTU),
+                               MakeUintegerAccessor (&SatNetDevice::SetMtu,
+                                                     &SatNetDevice::GetMtu),
+                               MakeUintegerChecker<uint16_t> ())
+                .AddAttribute ("Address",
+                               "The MAC address of this device.",
+                               Mac48AddressValue (Mac48Address ("ff:ff:ff:ff:ff:ff")),
+                               MakeMac48AddressAccessor (&SatNetDevice::m_address),
+                               MakeMac48AddressChecker ());
+        return tid;
 	}
 
     SatNetDevice::SatNetDevice():
@@ -34,6 +50,13 @@ NS_OBJECT_ENSURE_REGISTERED (SatNetDevice);
 
     }
 
+    void
+    SatNetDevice::SetDataRate (DataRate bps)
+    {
+        NS_LOG_FUNCTION (this);
+        m_bps = bps;
+    }
+
     Ptr<Channel>
     SatNetDevice::GetChannel(void) const {
         return m_channel;
@@ -41,18 +64,20 @@ NS_OBJECT_ENSURE_REGISTERED (SatNetDevice);
 
     void
     SatNetDevice::SetAddress(Address address) {
-
+        NS_LOG_FUNCTION (this << address);
+        m_address = Mac48Address::ConvertFrom (address);
     }
 
     uint16_t
     SatNetDevice::GetMtu(void) const {
-        return 0;
+        NS_LOG_FUNCTION (this);
+        return m_mtu;
     }
 
     Address
     SatNetDevice::GetAddress(void) const
     {
-        return Address();
+        return m_address;
     }
 
     bool
@@ -140,13 +165,14 @@ NS_OBJECT_ENSURE_REGISTERED (SatNetDevice);
     bool
     SatNetDevice::IsBroadcast(void) const
     {
-        return false;
+        return true;
     }
 
     Address
     SatNetDevice::GetBroadcast(void) const
     {
-        return Address();
+        NS_LOG_FUNCTION (this);
+        return Mac48Address ("ff:ff:ff:ff:ff:ff");
     }
 
     bool
@@ -165,7 +191,9 @@ NS_OBJECT_ENSURE_REGISTERED (SatNetDevice);
     bool
     SatNetDevice::SetMtu(const uint16_t mtu)
     {
-        return false;
+        NS_LOG_FUNCTION (this << mtu);
+        m_mtu = mtu;
+        return true;
     }
 
     uint32_t
@@ -186,4 +214,62 @@ NS_OBJECT_ENSURE_REGISTERED (SatNetDevice);
     {
         return false;
     }
+
+    void
+    SatNetDevice::DoDispose ()
+    {
+        NS_LOG_FUNCTION (this);
+        m_node = 0;
+        m_channel = 0;
+        m_currentPkt = 0;
+        m_queue = 0;
+        NetDevice::DoDispose ();
+    }
+
+    bool
+    SatNetDevice::TransmitStart (Ptr<Packet> p)
+    {
+        NS_LOG_FUNCTION (this << p);
+        NS_LOG_LOGIC ("UID is " << p->GetUid () << ")");
+
+        //
+        // This function is called to start the process of transmitting a packet.
+        // We need to tell the channel that we've started wiggling the wire and
+        // schedule an event that will be executed when the transmission is complete.
+        //
+        NS_ASSERT_MSG (m_txMachineState == READY, "Must be READY to transmit");
+        m_txMachineState = BUSY;
+        m_currentPkt = p;
+
+        return true;
+    }
+
+    void
+    SatNetDevice::TransmitComplete (void)
+    {
+        NS_LOG_FUNCTION (this);
+
+        //
+        // This function is called to when we're all done transmitting a packet.
+        // We try and pull another packet off of the transmit queue.  If the queue
+        // is empty, we are done, otherwise we need to start transmitting the
+        // next packet.
+        //
+        NS_ASSERT_MSG (m_txMachineState == BUSY, "Must be BUSY if transmitting");
+        m_txMachineState = READY;
+
+        NS_ASSERT_MSG (m_currentPkt != 0, "SatNetDevice::TransmitComplete()");
+
+        m_currentPkt = 0;
+
+        Ptr<Packet> p = m_queue->Dequeue ();
+        if (p == 0)
+        {
+            NS_LOG_LOGIC ("No pending packets in device queue after tx complete");
+            return;
+        }
+
+        TransmitStart (p);
+    }
+
 }
--- model/sat-net-device.h
+++ model/sat-net-device.h
@@ -1,6 +1,7 @@
 #ifndef SAT_NETDEVICE_H
 #define SAT_NETDEVICE_H
 
+#include <ns3/data-rate.h>
 #include "sat-channel.h"
 #include "ns3/net-device.h"
 #include "ns3/log.h"
@@ -64,6 +65,13 @@ public:
     void SetPromiscReceiveCallback (PromiscReceiveCallback cb);
     bool SupportsSendFrom (void) const;
 
+    void SatNetDevice::SetDataRate (DataRate bps);
+
+    /**
+   * \brief Dispose of the object
+   */
+    virtual void DoDispose (void);
+    
 private:
 
     /**
@@ -75,13 +83,65 @@ private:
         BUSY     /**< The transmitter is busy transmitting a packet */
     };
 
+    /**
+   * Start Sending a Packet Down the Wire.
+   *
+   * The TransmitStart method is the method that is used internally in the
+   * SatNetDevice to begin the process of sending a packet out on
+   * the channel.  The corresponding method is called on the channel to let
+   * it know that the physical device this class represents has virtually
+   * started sending signals.  An event is scheduled for the time at which
+   * the bits have been completely transmitted.
+   *
+   * \see SatNetDevice::TransmitStart ()
+   * \see TransmitComplete()
+   * \param p a reference to the packet to send
+   * \returns true if success, false on failure
+   */
+    bool TransmitStart (Ptr<Packet> p);
+
+    /**
+     * Stop Sending a Packet Down the Wire and Begin the Interframe Gap.
+     *
+     * The TransmitComplete method is used internally to finish the process
+     * of sending a packet out on the channel.
+     */
+    void TransmitComplete (void);
+
+    /**
+     * \brief Make the link up and running
+     *
+     * It calls also the linkChange callback.
+     */
+
+    /**
+    * The state of the Net Device transmit state machine.
+    * \see TxMachineState
+    */
     TxMachineState m_txMachineState;
 
+    /**
+    * The data rate that the Net Device uses to simulate packet transmission
+    * timing.
+    * \see class DataRate
+    */
+    DataRate m_bps;
     Ptr<Node> m_node; //!< Node owning this NetDevice
+    Mac48Address m_address;
     Ptr<SatChannel> m_channel;
     bool m_linkUp;      //!< Identify if the link is up or not
     Ptr<Packet> m_currentPkt; //!< Current packet processed
+    static const uint16_t DEFAULT_MTU = 1500; //!< Default MTU
 
+    /**
+     * \brief The Maximum Transmission Unit
+     *
+     * This corresponds to the maximum
+     * number of bytes that can be transmitted as seen from higher layers.
+     * This corresponds to the 1500 byte MTU size often seen on IP over
+     * Ethernet.
+     */
+    uint32_t m_mtu;
     /**
     * The Queue which this SatNetDevice uses as a packet source.
     * Management of this Queue has been delegated to the SatNetDevice
