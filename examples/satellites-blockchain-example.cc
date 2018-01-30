db2528dba2b442335ee741ae63bf2e35668955f6
Kirill Andreev <k.andreev@skoltech.ru>
2018-01-30 13:59:34 +0300

    Build fix

--- examples/satellites-blockchain-example.cc
+++ examples/satellites-blockchain-example.cc
@@ -13,30 +13,25 @@
 #include "ns3/csma-module.h"
 #include "ns3/tap-bridge-module.h"
 #include <ns3/v4ping-helper.h>
-#include <ns3/satellites-blockchain-helper.h>
+
+#include <ns3/satellites-helper.h>
 
 using namespace ns3;
 using namespace std;
 
-NS_LOG_COMPONENT_DEFINE ("SatellitesExample");
-
-int
-main (int argc, char *argv[]) {
+int main (int argc, char *argv[]) {
   LogComponentEnable ("TapBridge", LOG_LEVEL_INFO);
   LogComponentEnable ("ns3::SatelliteChannel", LOG_LEVEL_ALL);
   LogComponentEnable ("ns3::SatelliteNetDevice", LOG_LEVEL_ALL);
   LogComponentEnable ("SatellitesExample", LOG_LEVEL_INFO);
 
-  string mobilityTracePath;
-  string linksPath;
-  string linksTimeFrame;
-  uint32_t maxBytes = 0;
+  // Specify default paths for this toy example
+  string mobilityTracePath = "src/satellites-blockchain/examples/mobility.trace";
+  string linksPath = "satellites-blockchain/examples/links";
 
   CommandLine cmd;
-  cmd.AddValue ("maxBytes", "Total number of bytes for application to send", maxBytes);
   cmd.AddValue ("tracePath", "Path to the NS2Mobility trace file", mobilityTracePath);
   cmd.AddValue ("linksPath", "Path to the links", linksPath);
-  cmd.AddValue ("linksTimeFrame", "Links time frame", linksTimeFrame);
   cmd.Parse (argc,argv);
 
   //
@@ -47,24 +42,21 @@ main (int argc, char *argv[]) {
   GlobalValue::Bind ("SimulatorImplementationType", StringValue ("ns3::RealtimeSimulatorImpl"));
   GlobalValue::Bind ("ChecksumEnabled", BooleanValue (true));
 
-  NS_LOG_INFO ("Objects creation");
   NodeContainer nodes;
   SatellitesHelper satHelper;
-  uint32_t totalNodesAmount = 2;
-  nodes = satHelper.ConfigureNodes (totalNodesAmount, DataRate ("100MB/s"), Time (0));
+  uint32_t nNodes = 2;
+  // TODO: return net device container here
+  nodes = satHelper.ConfigureNodes (nNodes, DataRate ("100MB/s"), Time (0));
 
-  NS_LOG_INFO ("Mobility setup");
   Ns2MobilityHelper ns2 = Ns2MobilityHelper (mobilityTracePath);
   ns2.Install ();   // configure movements for each node, while reading trace file
 
   std::vector<SatelliteChannel::Links> links;
-  if(!linksPath.empty ()) {
-      links = satHelper.LoadLinks (linksPath, totalNodesAmount);
+  if(!linksPath.empty ())
+  {
+      links = satHelper.LoadLinks (linksPath, nNodes);
       satHelper.getM_channel ()->SetLinks (links);
-      NS_LOG_INFO ("Links was loaded!");
-    } else {
-      NS_LOG_INFO ("Links wasn't loaded!");
-    }
+  }
 
   //5. Install internet stack and routing:
   InternetStackHelper internet;
@@ -79,14 +71,14 @@ main (int argc, char *argv[]) {
   // 6. Use the TapBridgeHelper to connect to the pre-configured tap devices.
   TapBridgeHelper tapBridge;
   tapBridge.SetAttribute ("Mode", StringValue ("UseBridge"));
-  for(uint32_t i = 0; i < totalNodesAmount; i++) {
+  for(uint32_t i = 0; i < nNodes; i++)
+  {
       string deviceNameBase ("tap-");
       deviceNameBase.append (to_string (i + 1));
       tapBridge.SetAttribute ("DeviceName", StringValue (deviceNameBase));
       tapBridge.Install (nodes.Get (i), satHelper.getM_netDevices ().Get (i));
     }
 
-  //7. GO!
   lrr::GlobalGraph::Instance ()->Start ();
   Simulator::Stop (Seconds (650));
   Simulator::Run ();
@@ -94,7 +86,6 @@ main (int argc, char *argv[]) {
   lrr::GlobalGraph::Instance ()->Stop ();
 
   Simulator::Destroy ();
-  NS_LOG_INFO ("Done.");
   return 0;
 }
 
