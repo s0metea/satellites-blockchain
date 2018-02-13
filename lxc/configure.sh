1a55bd9a3c75003a65d40895d4799b9cbd049f6e
mikhailburov <mikhail.burov@skolktech.ru>
2018-02-14 02:23:22 +0300

    Unneeded files deletion

--- lxc/configure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# file: configure.sh
-for i in `seq 1 $1`;
-do
-    lxc-create -f ./lxc-conf/lxc-$i.conf -n lxc-$i -t ubuntu
-    lxc-start -n lxc-$i
-done
-# Show bridges with tap devices
-lxc-ls -f
